dd55fb0efd790c8d48ac2c43266edfa0776e8b7f
Anugesh <anugesh@Anugeshs-MacBook-Pro.local>
2019-03-10 17:53:13 -0400

    Commit from local

--- task.c
+++ task.c
@@ -1,15 +1,4 @@
 /****************************************task.c*****************************************
- Student Name: Anugesh Balasubramaniam  Student Number: 0883563
- Date: 09/28/2018                    Course Name: CIS*3090
- I have exclusive control over this submission via my password.
- By including this statement in this header comment, I certify that:
- 1) I have read and understood the University policy on academic integrity;
- 2) I have completed the Computing with Integrity Tutorial on Moodle; and
- 3) I have achieved at least 80% in the Computing with Integrity Self Test.
- I assert that this work is my own. I have appropriately acknowledged any and all material
- (data, images, ideas or words) that I have used, whether directly quoted or paraphrased.
- Furthermore, I certify that this assignment was prepared by me specifically for this course.
- 
  Compile & run program
  -   make
  -  ./task -s <number> -t <number> -o    OR    ./task -s <number> -t <number>
